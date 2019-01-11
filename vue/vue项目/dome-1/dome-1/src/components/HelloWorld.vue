c69be617358bab74803443e3de822f3559cf32d5
wang787426 <13512440895@163.com>
2019-01-11 21:37:35 +0800

    php

---
+++ vue/vue项目/dome-1/dome-1/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <div>woowowowowoow</div>
+      <span>wan</span>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
